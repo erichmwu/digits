063880af8e8f699cc9e772c10888da9432940c37
Eric Wu <eric.ics211@gmail.com>
2015-03-12 18:26:13 -1000

    Answer to E42

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,9 @@
 package controllers;
 
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.formdata.ContactFormData;
 import views.html.Index;
 import views.html.NewContact;
 
@@ -12,6 +14,7 @@ public class Application extends Controller {
 
   /**
    * Returns the home page.
+   *
    * @return The resulting home page.
    */
   public static Result index() {
@@ -20,11 +23,24 @@ public class Application extends Controller {
 
   /**
    * Returns the New Contact page.
+   *
    * @return The New Contact page.
    */
   public static Result newContact() {
-    return ok(NewContact.render("Welcome to New Contact."));
+    Form<views.formdata.ContactFormData> formData = Form.form(ContactFormData.class);
+    return ok(NewContact.render(formData));
+  }
 
+  /**
+   * Gets the form data from the New Contact page.
+   *
+   * @return The data from the form on the New Contact page.
+   */
+  public static Result postRequest() {
+    Form<views.formdata.ContactFormData> formData = Form.form(ContactFormData.class).bindFromRequest();
+    ContactFormData data = formData.get();
+    System.out.println("Data " + data.firstName + ", " + data.lastName + ", " + data.telephone);
+    return ok(NewContact.render(formData));
   }
 
 }
---
+++ app/views/formdata/ContactFormData.java
@@ -0,0 +1,13 @@
+package views.formdata;
+
+/**
+ * Backing class for form data.
+ */
+public class ContactFormData {
+  /** First name. */
+  public String firstName = "";
+  /** Last name. */
+  public String lastName = "";
+  /** Telephone number. */
+  public String telephone = "";
+}
